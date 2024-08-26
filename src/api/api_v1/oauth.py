83fb08447dc6211d966d2493d109c046214d79ed
Semolik <70706334+Semolik@users.noreply.github.com>
2024-08-27 00:50:57 +0700

    Update oauth.py

--- src/api/api_v1/oauth.py
+++ src/api/api_v1/oauth.py
@@ -1,8 +1,10 @@
+import aiohttp
+from fastapi.responses import RedirectResponse
 from src.schemas.users import UserRead
 from src.users_controller import auth_backend, fastapi_users
 from httpx_oauth.clients.google import GoogleOAuth2
 from httpx_oauth.clients.github import GitHubOAuth2
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Response
 from src.core.config import settings
 
 google_oauth_client = GoogleOAuth2(
@@ -35,11 +37,20 @@ oauth_router.include_router(
         auth_backend,
         settings.SECRET,
         associate_by_email=True,
-        redirect_url=f"{settings.API_DOMAIN}/api/v1/auth/github/callback",
+        redirect_url=f"{settings.API_DOMAIN}/api/v1/auth/github/callback/redirect",
         is_verified_by_default=True,
     ),
     prefix="/github",
 )
+
+
+@oauth_router.get("/github/callback/redirect", response_class=RedirectResponse)
+async def github_callback_redirection(request: Request, response: Response):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{settings.API_DOMAIN}/api/v1/auth/github/callback", params=request.query_params) as response_callback:
+            response.set_cookie(key="fastapiusersauth",
+                                value=response_callback.cookies.get("fastapiusersauth"))
+            return RedirectResponse(url=settings.FRONTEND_DOMAIN)
 oauth_router.include_router(
     fastapi_users.get_oauth_associate_router(
         github_oauth_client, UserRead, settings.SECRET, redirect_url=settings.FRONTEND_DOMAIN),
