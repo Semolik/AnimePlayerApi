382ac3d19b3850a128dc9542846e91606c973c2a
Semolik <70706334+Semolik@users.noreply.github.com>
2024-09-05 00:55:31 +0700

    added local swagger bundle

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from pathlib import Path
+from fastapi import FastAPI, applications
+from fastapi.staticfiles import StaticFiles
 from starlette.middleware.cors import CORSMiddleware
-
 from src.redis.containers import Container
 from src.api.api_v1.api import api_router
 from src.core.config import settings
@@ -9,10 +10,23 @@ from src.db.init import init_superuser
 from src.db.session import create_db_and_tables
 from src.utils.files import init_folders
 import src.models.event_watcher
+from fastapi.openapi.docs import get_swagger_ui_html
 
 app = FastAPI(title=settings.PROJECT_NAME,
               openapi_url=f"{settings.API_V1_STR}/openapi.json")
+print(Path("static/swagger-ui.css").exists())
+if Path("static/swagger-ui.css").exists() and Path("static/swagger-ui-bundle.js").exists():
+    app.mount("/assets", StaticFiles(directory='static'), name="static")
 
+    def swagger_monkey_patch(*args, **kwargs):
+        return get_swagger_ui_html(
+            *args,
+            **kwargs,
+            swagger_favicon_url="",
+            swagger_css_url="/assets/swagger-ui.css",
+            swagger_js_url="/assets/swagger-ui-bundle.js",
+        )
+    applications.get_swagger_ui_html = swagger_monkey_patch
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
